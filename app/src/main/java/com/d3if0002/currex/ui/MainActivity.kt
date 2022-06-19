eee66ede362d2b485da7a7102a84bf9db1052fd4
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-19 21:41:22 +0700

    feat: membuat notification channel

--- app/src/main/java/com/d3if0002/currex/ui/MainActivity.kt
+++ app/src/main/java/com/d3if0002/currex/ui/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.d3if0002.currex.ui
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.getSystemService
 import com.d3if0002.currex.R
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val CHANNEL_ID = "updater"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
     }
 }
