340d78741fb4ab708088598bef154ceb1c23f7e7
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-19 11:27:37 +0700

    feat: membuat UI crypto
    membuat UI layout halaman crypto, menambahkan viewbinding di class fragment_crypto.xml

--- app/src/main/java/com/d3if0002/currex/ui/crypto/CryptoFragment.kt
+++ app/src/main/java/com/d3if0002/currex/ui/crypto/CryptoFragment.kt
@@ -6,13 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.d3if0002.currex.R
+import com.d3if0002.currex.databinding.FragmentCryptoBinding
 
 class CryptoFragment : Fragment() {
+    private var _binding: FragmentCryptoBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crypto, container, false)
+    ): View {
+        _binding = FragmentCryptoBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
