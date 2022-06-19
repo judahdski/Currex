9dd335d9c0cd055dd6126cde65960067f3bd153b
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-19 17:16:29 +0700

    feat: membuat UI convert currency
    membuat UI layout halaman convert currency, menambahkan viewbinding di class fragment_convert_currency.xml, casting untuk semua component

--- app/src/main/java/com/d3if0002/currex/ui/convertCurrency/ConvertCurrencyFragment.kt
+++ app/src/main/java/com/d3if0002/currex/ui/convertCurrency/ConvertCurrencyFragment.kt
@@ -5,14 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.d3if0002.currex.R
+import com.d3if0002.currex.databinding.FragmentConvertCurrencyBinding
 
-class ConvertCurrencyFragment : Fragment() {
+class ConvertCurrencyFragment : Fragment(), View.OnClickListener {
+
+    private var _binding: FragmentConvertCurrencyBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_convert_currency, container, false)
+    ): View {
+        _binding = FragmentConvertCurrencyBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUI()
+    }
+
+    private fun setUI() {
+        val fromEditText = binding.fromCurrencyTf
+        val toEditText = binding.toCurrencyTf
+        val amountEditText = binding.amountCurrencyTf
+        val convertBtn = binding.convertCurrencyBtn
+
+        convertBtn.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("call convert currency service function")
     }
 }
