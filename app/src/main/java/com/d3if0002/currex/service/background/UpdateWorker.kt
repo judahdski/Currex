23caeb5cd3bfaace8b75e86a90c1f654e81dee5d
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-19 22:24:42 +0700

    feat: membuat notification
    membuat notif builder, menerapkan intent ke notif agar bisa diklik dan masuk ke main activity

--- app/src/main/java/com/d3if0002/currex/service/background/UpdateWorker.kt
+++ app/src/main/java/com/d3if0002/currex/service/background/UpdateWorker.kt
@@ -1,19 +1,38 @@
 package com.d3if0002.currex.service.background
 
+import android.app.PendingIntent
 import android.content.Context
-import android.util.Log
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.d3if0002.currex.R
+import com.d3if0002.currex.ui.MainActivity
 
 class UpdateWorker(
     context: Context,
     workerParams: WorkerParameters
 ) :
     Worker(context, workerParams) {
-    override fun doWork(): Result {
-        Log.d("Debugss", "Background task berhasil dijalankan")
 
-        // TODO: Create a notification
+    private val notificationId = 44
+
+    override fun doWork(): Result {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(applicationContext, MainActivity.CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(applicationContext.getString(R.string.notif_title))
+            .setContentText(applicationContext.getString(R.string.notif_text))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val manager = NotificationManagerCompat.from(applicationContext)
+
+        manager.notify(notificationId, builder.build())
 
         return Result.success()
     }
